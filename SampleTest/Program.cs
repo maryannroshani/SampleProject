68bc9db190b84ffa1ee1608c9c1ee0d4a0c6c925
Maryann Roshani <maryann.roshani@gmail.com>
2019-04-27 12:01:09 +1200

    initial commit

---
+++ SampleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build a calculator 
+
+            Console.WriteLine("Enter Number one");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number two");
+            int numberTwo = int.Parse(Console.ReadLine());
+            int answer = numberOne + numberTwo;  
+           
+            Console.WriteLine("The answer is {0} + {1} = {2}", numberOne, numberTwo, answer);
+            Console.ReadLine();
+        }
+    }
+}
