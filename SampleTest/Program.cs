1637814500f57689da370a22978736778c5a2068
Maryann Roshani <maryann.roshani@gmail.com>
2019-04-28 09:04:44 +1200

    if-else

--- SampleTest/Program.cs
+++ SampleTest/Program.cs
@@ -12,14 +12,23 @@ namespace SampleTest
         {
             //build a calculator 
 
-            Console.WriteLine("Enter Number one");
-            int numberOne = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Number two");
-            int numberTwo = int.Parse(Console.ReadLine());
-            int answer = numberOne + numberTwo;  
-           
-            Console.WriteLine("The answer is {0} + {1} = {2}", numberOne, numberTwo, answer);
+            //Console.WriteLine("Enter Number one");
+            //int numberOne = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Number two");
+            //int numberTwo = int.Parse(Console.ReadLine());
+            //int answer = numberOne + numberTwo;            
+            //Console.WriteLine("The answer is {0} + {1} = {2}", numberOne, numberTwo, answer);
+            //Console.ReadLine();
+
+            //total amount of time in minutes only
+
+            Console.WriteLine("Enter the time in minutes only");
+            int toalMints = int.Parse(Console.ReadLine());
+            int mintsHours = toalMints / 60;
+            int leftMins = toalMints % 60;
+            Console.WriteLine("Total amount of time is {0} minutes and {1} hour and {2} minutes", toalMints,mintsHours,leftMins );
             Console.ReadLine();
+
         }
     }
 }
